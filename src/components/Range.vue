c6ae6ef9ad8d5680dcdf471dd03c3c4f252f9343
Fábio Gonçalves <fabio.g@inttecnologia.com.br>
2019-02-19 09:58:48 -0300

    Ajuste label valor salário

--- src/components/Range.vue
+++ src/components/Range.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <label>{{customLabel}}</label>
-    <input type="range" v-bind="$attrs" :class="inputClasses" :value="valor" @change="atualizar">
+    <input type="range" v-bind="$attrs" :class="inputClasses"     
+    :value="valor" @change="atualizar">
   </div>
 </template>
 
@@ -17,19 +18,20 @@ export default {
     valor: [Number, String],
     inputClasses: [String, Object, Array]
   },
-  data() {
-    return { valorAtual: this.valor || this.$attrs.min}
-  },
+ // data() {
+ //   return { valorAtual: this.valor || this.$attrs.min}
+ // },//comentado porque não atualizava a label no formulario
   computed: {
     customLabel() {
-      return `${this.label} (R$ ${this.valorAtual})`;
+     // return `${this.label} (R$ ${this.valorAtual})`; comentado porque não atualizava a label no formulario
+     return `${this.label} (R$ ${this.valor || this.$attrs.min})`;
     }
   },
   methods: {
     atualizar(event ) {
         const valor = event.target.value
         this.$emit('change',valor)
-        this.valorAtual = valor
+        //this.valorAtual = valor
     }
   },
   created() {}
